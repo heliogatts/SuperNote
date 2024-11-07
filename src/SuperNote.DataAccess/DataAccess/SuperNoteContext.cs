f5b42c90c66cf5820f88049d460ad198cb1e7a45
Hélio Gatts Junior <heliogatts@gmail.com>
2024-11-06 21:10:39 -0300

    Merge pull request #3
    * basic implementation of the infrastructure layer

---
+++ src/SuperNote.DataAccess/DataAccess/SuperNoteContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperNote.Domain.Notes;
+
+namespace SuperNote.DataAccess.DataAccess;
+
+public class SuperNoteContext : DbContext
+{
+    public SuperNoteContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Note> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SuperNoteContext).Assembly);
+    }
+}
---
+++ src/SuperNote.DataAccess/DataAccess/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using SuperNote.Domain.Abstractions.Aggregates;
+using SuperNote.Domain.Abstractions.DataAccess;
+using SuperNote.Domain.Abstractions.DomainEvents;
+
+namespace SuperNote.DataAccess.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly IPublisher _publisher;
+    private readonly SuperNoteContext _context;
+
+    public UnitOfWork(IPublisher publisher, SuperNoteContext context)
+    {
+        _publisher = publisher;
+        _context = context;
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEntities = GetDomainEntities(_context);
+        var domainEvents = GetDomainEvents(domainEntities);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        // dispatching domain events
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent, cancellationToken);
+        }
+        
+        domainEntities.ForEach(entity => entity.ClearDomainEvents());
+        
+        List<AggregateRoot> GetDomainEntities(SuperNoteContext context) => 
+            context.ChangeTracker
+                .Entries<AggregateRoot>()
+                .Select(entry => entry.Entity)
+                .ToList();
+        
+        List<IDomainEvent> GetDomainEvents(List<AggregateRoot> entities) =>
+            entities
+                .Where(entity => entity.DomainEvents.Any())
+                .SelectMany(entity => entity.DomainEvents)
+                .ToList();
+    }
+}
