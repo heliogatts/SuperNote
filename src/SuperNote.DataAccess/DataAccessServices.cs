99e44e8989051e3ba7adcec8ced3cf379269cfef
Hélio Gatts Junior <heliogatts@gmail.com>
2024-11-06 21:09:33 -0300

    basic implementation of the infrastructure layer

--- src/SuperNote.DataAccess/DataAccessServices.cs
+++ src/SuperNote.DataAccess/DataAccessServices.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SuperNote.DataAccess.DataAccess;
 
 namespace SuperNote.DataAccess;
 
 public static class DataAccessServices
 {
-    public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    private const string InMemoryDatabaseName = "Data Source=./SuperNoteDatabase.db;";
+        
+    public static IServiceCollection AddDataAccessServices(
+        this IServiceCollection services, string? connectionString = null)
     {
+        IsConnectionStringAvailable(services, connectionString);
         return services;
     }
+
+    #region Private Methods
+
+    private static void IsConnectionStringAvailable(IServiceCollection services, string? connectionString)
+    {
+        if (connectionString is not null)
+        {
+            services.AddDbContext<SuperNoteContext>(options =>
+            {
+                options.UseSqlite(InMemoryDatabaseName);
+            });
+        }
+        else
+        {
+            services.AddDbContext<SuperNoteContext>(options =>
+            {
+                options.UseInMemoryDatabase(InMemoryDatabaseName);
+            });
+        }
+    }
+    #endregion
+    
 }
---
+++ src/SuperNote.DataAccess/Notes/NoteEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperNote.Domain.Notes;
+
+namespace SuperNote.DataAccess.Notes;
+
+public class NoteEntityTypeConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder
+            .HasKey(b => b.Id);
+        
+        builder
+            .Property(e => e.Id)
+            .HasConversion(id => id.Value, value => new NoteId(value));
+
+        builder
+            .Property(e => e.NoteText)
+            .HasConversion(e => e.Value, val => NoteText.Create(val).Value);
+
+        builder
+            .Property(x => x.LastModified);
+
+    }
+}
